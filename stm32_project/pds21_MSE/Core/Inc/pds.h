9a1ea98daf8c8574cb49926003ef74b433501079
unknown <monzonlucasfabricio@gmail.com>
2023-09-08 14:31:14 -0300

    Add pds21_MSE with it changes

---
+++ stm32_project/pds21_MSE/Core/Inc/pds.h
@@ -0,0 +1,33 @@
+/*
+ * pds.h
+ *
+ *  Created on: Aug 22, 2023
+ *      Author: lab
+ */
+
+#ifndef INC_PDS_H_
+#define INC_PDS_H_
+
+#include "stdio.h"
+#include "stdint.h"
+#include "stdlib.h"
+#include "stdbool.h"
+#include "usart.h"
+#include "stm32f429xx.h"
+#include "arm_math.h"
+
+#define CLOCK_SPEED 168000000
+// Read Cycles Counter
+#define DBG_CyclesCounterRead()		DWT->CYCCNT
+// Reset Cycles Counter
+#define DBG_CyclesCounterReset()    (DWT->CYCCNT = 0)
+
+/* Function prototypes */
+void trigger(int16_t threshold);
+uint16_t ADC_Read(uint16_t adc);
+bool DBG_CyclesCounterInit( uint32_t clockSpeed );
+void uartWriteByteArray( UART_HandleTypeDef *huart, const uint8_t* byteArray, uint32_t byteArrayLen );
+void gpioToggle(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
+void DAC_Write(DAC_HandleTypeDef *handle, uint16_t value);
+
+#endif /* INC_PDS_H_ */
